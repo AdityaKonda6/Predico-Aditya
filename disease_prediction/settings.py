bfc9ef7d436da995515cf25bbcd735cd3fdfb2e0
Aditya Konda <adityakonda04@gmail.com>
2024-07-09 12:48:33 +0530

    modified:   disease_prediction/__pycache__/settings.cpython-311.pyc 	modified:   disease_prediction/settings.py

--- disease_prediction/settings.py
+++ disease_prediction/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -123,9 +124,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = '/opt/build/repo/static'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 
     os.path.join(BASE_DIR,'templates')
 ]
+
