77f3ed2c9b3c07691d4621fe3d2236903a1765f5
Aditya Konda <adityakonda04@gmail.com>
2024-07-09 13:22:19 +0530

    modified:   disease_prediction/__pycache__/settings.cpython-311.pyc 	modified:   disease_prediction/settings.py

--- disease_prediction/settings.py
+++ disease_prediction/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = '/opt/build/repo/static'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
