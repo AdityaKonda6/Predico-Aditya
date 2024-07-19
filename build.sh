c2698d36b84918e628a6779669fe22859a82d4ae
Aditya Konda <95713208+AdityaKonda6@users.noreply.github.com>
2024-07-19 12:22:17 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-curl https://www.python.org/ftp/python/3.9.7/Python-3.9.7.tgz | tar xz
-cd Python-3.9.7
-./configure --enable-optimizations
-make -j 8
-sudo make altinstall
-cd ..
-python3.9 -m pip install -r requirements.txt
+python -m pip install --upgrade pip
+pip install -r requirements.txt
